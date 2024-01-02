e7aeef1f75f01a227879fb94ef8610154ac0a137
jayem <dagzz23123456789@gmail.com>
2024-01-02 23:29:15 +0800

    asd12312453

--- dbh/dbh.inc.php
+++ dbh/dbh.inc.php
@@ -6,6 +6,7 @@ $dbname = "barangay";
 
 $conn = mysqli_connect($localhost, $username, $password, $dbname);
 if (!$conn) {
+    echo "<script>alert('tanga');</script>";
  die ('Fail to connect to MySQL: ' . mysqli_connect_error());
 }
 
